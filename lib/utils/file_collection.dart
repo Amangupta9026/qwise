87ca26eaa3cef948975e3ee69b81fe897e2a7ecc
Pratap Singh <ps6067966@gmail.com>
2023-05-19 12:32:56 +0530

    text style file name change.

--- lib/utils/file_collection.dart
+++ lib/utils/file_collection.dart
@@ -1,7 +1,7 @@
 export 'package:flutter/material.dart';
 export '../../utils/colors.dart';
 export '../auth/signup/signup_screen.dart';
-export 'package:qwise/widget/textStyle.dart';
+export 'package:qwise/widget/text_style.dart';
 export 'package:permission_handler/permission_handler.dart';
 export 'package:qwise/utils/utils.dart';
 export 'package:qwise/auth/on_boarding_screen.dart/on_boarding_screen.dart';
