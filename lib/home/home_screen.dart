ebd003f6fbff0863a9b60256935ca32f47a03179
Pratap Singh <ps6067966@gmail.com>
2023-05-19 18:46:30 +0530

    home to stateless.

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,13 +1,8 @@
 import 'package:qwise/utils/file_collection.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
