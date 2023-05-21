5c3b4b55cd0ccde80c1fcd17299216aa9b7a14f5
Pratap Singh <ps6067966@gmail.com>
2023-05-21 13:51:38 +0530

    logout

--- lib/profile/profile_screen.dart
+++ lib/profile/profile_screen.dart
@@ -1,15 +1,38 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: use_build_context_synchronously
+
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:qwise/utils/file_collection.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
+  logout(BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      log("e");
+    }
+    context.pushNamed(RouteNames.signInScreen);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text('ProfileScreen'),
-        ],
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  logout(context);
+                },
+                child: const Text('Logout'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
