44c3b555579c4c052160e08f8b21630a4ba317e5
Pratap Singh <ps6067966@gmail.com>
2023-05-24 11:13:38 +0530

    courses.

--- lib/home/recommended_courses/recomended_courses.dart
+++ lib/home/recommended_courses/recomended_courses.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:qwise/utils/file_collection.dart';
 
+import '../enrolled_courses.dart';
+
 class RecommendedCourses extends StatelessWidget {
   RecommendedCourses({super.key});
 
@@ -25,151 +27,93 @@ class RecommendedCourses extends StatelessWidget {
             fontWeight1: FontWeight.w600,
           ),
         ),
-        Stack(
-          children: [
-            InkWell(
-              onTap: () {
-                context.pushNamed(RouteNames.courseDetails);
-              },
-              child: Card(
-                margin: EdgeInsets.zero,
-                child: Container(
-                  padding: EdgeInsets.zero,
-                  height: MediaQuery.of(context).size.height * 0.30,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(10.0, 10, 10, 0),
+        StreamBuilder(
+            stream: courseCollection.snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              return InkWell(
+                onTap: () {
+                  context.pushNamed(RouteNames.courseDetails);
+                },
+                child: Card(
+                  margin: EdgeInsets.zero,
+                  child: Container(
+                    padding: EdgeInsets.zero,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Image.asset(
-                          'assets/images/course1.png',
-                          width: double.infinity,
-                          fit: BoxFit.cover,
+                        Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Image.asset(
+                              'assets/images/course1.png',
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                              height: 130,
+                            ),
+                            Positioned.fill(
+                              bottom: -20,
+                              right: 20,
+                              child: Align(
+                                alignment: Alignment.bottomRight,
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: const Text("View Details"),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const TextWidget(
+                                text1: 'Learn Principle of UX & U...',
+                                color1: primaryColor,
+                                size1: 18,
+                                fontWeight1: FontWeight.bold,
+                              ),
+                              Row(
+                                children: [
+                                  RatingBar.builder(
+                                    ignoreGestures: true,
+                                    initialRating: 5.0,
+                                    minRating: 1,
+                                    direction: Axis.horizontal,
+                                    allowHalfRating: true,
+                                    itemCount: 5,
+                                    itemSize: 16,
+                                    itemPadding: const EdgeInsets.symmetric(
+                                        horizontal: 1.0),
+                                    itemBuilder: (context, _) => const Icon(
+                                      Icons.star,
+                                      color: yellowColor,
+                                      size: 2,
+                                    ),
+                                    onRatingUpdate: (rating) {},
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ],
                     ),
                   ),
                 ),
-              ),
-            ),
-            Positioned.fill(
-              top: 90.0,
-              left: 30.0,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Course Name',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 10.0),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                              decoration: BoxDecoration(
-                                color: primaryColor,
-                                borderRadius: BorderRadius.circular(20),
-                                boxShadow: const [
-                                  BoxShadow(color: Colors.grey, blurRadius: 10)
-                                ],
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(children: [
-                                  for (int i = 0; i < images!.length; i++)
-                                    images?[i],
-                                ]),
-                              )),
-
-                          // another container for details
-                          Container(
-                              margin: const EdgeInsets.only(right: 30),
-                              decoration: BoxDecoration(
-                                color: primaryColor,
-                                borderRadius: BorderRadius.circular(12),
-                                boxShadow: const [
-                                  BoxShadow(color: Colors.grey, blurRadius: 10)
-                                ],
-                              ),
-                              child: const Padding(
-                                  padding: EdgeInsets.all(10.0),
-                                  child: TextWidget(
-                                    text1: 'View Details',
-                                    color1: Colors.white,
-                                  )))
-                        ]),
-                    const SizedBox(height: 10.0),
-                    Row(
-                      children: [
-                        const TextWidget(
-                          text1: 'Learn Principle of UX & U...',
-                          color1: primaryColor,
-                          size1: 18,
-                          fontWeight1: FontWeight.bold,
-                        ),
-                        const SizedBox(width: 10.0),
-                        const TextWidget(
-                          text1: 'Live',
-                          color1: primaryColor,
-                          size1: 18,
-                          fontWeight1: FontWeight.w600,
-                        ),
-                        const SizedBox(width: 4.0),
-                        Container(
-                            height: 6,
-                            width: 6,
-                            decoration: const BoxDecoration(
-                                color: primaryColor, shape: BoxShape.circle))
-                      ],
-                    ),
-                    const SizedBox(height: 4.0),
-                    Row(
-                      children: [
-                        const TextWidget(
-                          text1: 'Rs 3000/-',
-                          color1: primaryColor,
-                          size1: 16,
-                          fontWeight1: FontWeight.bold,
-                        ),
-                        const SizedBox(width: 10.0),
-                        RatingBar.builder(
-                          ignoreGestures: true,
-                          initialRating: 5.0,
-                          minRating: 1,
-                          direction: Axis.horizontal,
-                          allowHalfRating: true,
-                          itemCount: 5,
-                          itemSize: 16,
-                          itemPadding:
-                              const EdgeInsets.symmetric(horizontal: 1.0),
-                          itemBuilder: (context, _) => const Icon(
-                            Icons.star,
-                            color: yellowColor,
-                            size: 2,
-                          ),
-                          onRatingUpdate: (rating) {},
-                        ),
-                        const SizedBox(width: 10.0),
-                        const TextWidget(
-                          text1: '4.7 (1.0k)',
-                          color1: primaryColor,
-                          size1: 16,
-                          fontWeight1: FontWeight.bold,
-                        )
-                      ],
-                    )
-                  ]),
-            ),
-          ],
-        ),
+              );
+            }),
       ],
     );
   }
